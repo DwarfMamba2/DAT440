828fd28edb532752142f7ea2ea0c252e3bd906ab
DwarfMamba2 <oscpal@student.chalmers.se>
2023-05-16 14:30:14 +0200

    epsilon-greedy

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 
 class Agent(object):
@@ -5,11 +6,21 @@ class Agent(object):
     def __init__(self, state_space, action_space):
         self.action_space = action_space
         self.state_space = state_space
+        self.q_table = np.zeros((self.state_space, self.action_space))
+        self.gamma = 0.95
+        self.epsilon = 0.05
+
 
     def observe(self, observation, reward, done):
         #Add your code here
+
         pass
     def act(self, observation):
         #Add your code here
+        if random.uniform(0,1) < self.epsilon:
+            action = np.random.randint(self.action_space)
+        else:
+            action = np.argmax(self.q_table[observation,:])
+
 
         return np.random.randint(self.action_space)
